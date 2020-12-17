7d818459ddc6e9e69dac5396d3c9a89402f4e59e
pribula329 <72249242+pribula329@users.noreply.github.com>
2020-12-17 15:14:17 +0100

    Lukas uprava casu

--- javascript/javascriptCode.js
+++ javascript/javascriptCode.js
@@ -7,12 +7,28 @@ let datetime = currentdate.getDate() + "/"
     + currentdate.getSeconds();
 function updateDate(){
     currentdate = new Date();
+    var minuty;
+    if(currentdate.getMinutes()<10){
+        minuty="0"+ currentdate.getMinutes();
+    }
+    else {
+        minuty = currentdate.getMinutes();
+    }
+
+    var sekundy;
+    if(currentdate.getSeconds()<10){
+        sekundy="0"+ currentdate.getSeconds();
+    }
+    else {
+        sekundy = currentdate.getSeconds();
+    }
+
     datetime = currentdate.getDate() + "/"
         + (currentdate.getMonth()+1)  + "/"
         + currentdate.getFullYear() + "  "
         + currentdate.getHours() + ":"
-        + currentdate.getMinutes() + ":"
-        + currentdate.getSeconds();
+        + minuty + ":"
+        + sekundy;
 }
 console.log(datetime);
 
