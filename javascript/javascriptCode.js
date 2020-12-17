39250c135f4d1a5080554a57433099c9d7fa3842
pribula329 <72249242+pribula329@users.noreply.github.com>
2020-12-17 15:29:38 +0100

    Lukas uprava casu

--- javascript/javascriptCode.js
+++ javascript/javascriptCode.js
@@ -5,23 +5,37 @@ let datetime = currentdate.getDate() + "/"
     + currentdate.getHours() + ":"
     + currentdate.getMinutes() + ":"
     + currentdate.getSeconds();
+
+var minuty;
+function upravaMin(currentMin){
+
+    minuty;
+    if(currentMin.getMinutes()<10){
+        minuty="0"+ currentMin.getMinutes();
+    }
+    else {
+        minuty = currentMin.getMinutes();
+    }
+}
+
+var sekundy;
+function upravaSec(currentSec){
+
+    sekundy;
+    if(currentSec.getSeconds()<10){
+        sekundy="0"+ currentSec.getSeconds();
+    }
+    else {
+        sekundy = currentSec.getSeconds();
+    }
+}
+
+
 function updateDate(){
     currentdate = new Date();
-    var minuty;
-    if(currentdate.getMinutes()<10){
-        minuty="0"+ currentdate.getMinutes();
-    }
-    else {
-        minuty = currentdate.getMinutes();
-    }
 
-    var sekundy;
-    if(currentdate.getSeconds()<10){
-        sekundy="0"+ currentdate.getSeconds();
-    }
-    else {
-        sekundy = currentdate.getSeconds();
-    }
+    upravaMin(currentdate)
+    upravaSec(currentdate)
 
     datetime = currentdate.getDate() + "/"
         + (currentdate.getMonth()+1)  + "/"
