9e0f4d308f3a8f9ba2372b741fcdbe3f881eff3e
Peter Rigo <boys4849@gmail.com>
2020-12-18 01:58:13 +0100

    Added namesDays,Fixed small bugs

--- javascript/javascriptCode.js
+++ javascript/javascriptCode.js
@@ -1,12 +1,90 @@
-let currentdate  = new Date();
-let datetime = currentdate.getDate() + "/"
-    + (currentdate.getMonth()+1)  + "/"
-    + currentdate.getFullYear() + "  "
-    + currentdate.getHours() + ":"
-    + currentdate.getMinutes() + ":"
-    + currentdate.getSeconds();
+let currentdate=null;
+let hodiny,minuty,sekundy;
+let datetime=null;
+// date
+updateDate()
+document.getElementById("time").innerHTML = datetime;
 
-var minuty;
+setInterval(function(){
+    updateDate();
+    document.getElementById("time").innerHTML = datetime;}, 1000);
+
+// xml reading
+let xhttp,xmlDoc,x,input,size,SKsviatky,SK,divText,startString;
+searchXML((currentdate.getMonth()+1).toString()+currentdate.getDate().toString());
+//
+
+function loadXMLDoc(dname)
+{
+
+    if (window.XMLHttpRequest)
+    {
+        xhttp=new XMLHttpRequest();
+    }
+    else
+    {
+        xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttp.open("GET",dname,false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+function searchXML(date)
+{
+    xmlDoc=loadXMLDoc("../xmlFiles/meniny.xml");
+    x=xmlDoc.getElementsByTagName("zaznam");
+    input = date;
+    size = input.length;
+
+    if (input === "")
+    {
+        //nenasiel sa input
+        return false;
+    }
+    for (let i=0;i<x.length;i++)
+    {
+        startString = x[i].children[0].textContent;
+
+        if (startString.toLowerCase() === input.toLowerCase())
+        {
+            if(x[i].children[1].tagName==="SKsviatky"){SKsviatky = x[i].children[1].textContent;}
+            else if(x[i].children[2].tagName==="SKsviatky"){SKsviatky = x[i].children[2].textContent;}
+            else if(x[i].children[3].tagName==="SKsviatky"){SKsviatky = x[i].children[3].textContent;}
+            if(x[i].children[1].tagName==="SK"){
+                SK=x[i].children[1].textContent;
+            }else
+            {
+                divText = "nikto";
+                break;
+            }
+            divText = SK;
+            break;
+        }
+        else
+        {
+            divText = "nikto";
+        }
+    }
+    if(divText==="nikto"){
+        document.getElementById("nameDay").innerHTML = 'Dnes nemá nikto meniny';
+    }else{
+        document.getElementById("nameDay").innerHTML = 'Meniny má <span class="name">'+divText+'</span>';
+    }
+
+}
+
+//,,,,,,,,,,,,
+
+function upravaHod(currentHour){
+    hodiny;
+    if(currentHour.getHours()<10){
+        hodiny="0"+ currentHour.getHours();
+    }
+    else {
+        hodiny = currentHour.getHours();
+    }
+}
 function upravaMin(currentMin){
 
     minuty;
@@ -18,7 +96,6 @@ function upravaMin(currentMin){
     }
 }
 
-var sekundy;
 function upravaSec(currentSec){
 
     sekundy;
@@ -34,22 +111,21 @@ function upravaSec(currentSec){
 function updateDate(){
     currentdate = new Date();
 
+    upravaHod(currentdate)
     upravaMin(currentdate)
     upravaSec(currentdate)
 
     datetime = currentdate.getDate() + "/"
         + (currentdate.getMonth()+1)  + "/"
         + currentdate.getFullYear() + "  "
-        + currentdate.getHours() + ":"
+        + hodiny + ":"
         + minuty + ":"
         + sekundy;
-}
-console.log(datetime);
-
-setInterval(function(){
-    updateDate();
     console.log(datetime);
-    document.getElementById("time").innerHTML = datetime;}, 1000);
+}
+
+
+
 
 
 document.getElementById("eGame").addEventListener('mousemove', function (){
