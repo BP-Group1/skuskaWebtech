b32fbf29029f169a7a44015fed3ba7b5c6e71ac1
pribula329 <72249242+pribula329@users.noreply.github.com>
2020-12-19 15:02:36 +0100

    Lukas pridanie profiloviek

--- javascript/javascriptCode.js
+++ javascript/javascriptCode.js
@@ -162,3 +162,78 @@ document.getElementById("pGame").addEventListener('mousemove', function (){
 document.getElementById("pGame").addEventListener('mouseleave', function (){
     document.getElementById("peterRigo").style.display = "none";
 });
+
+//kod nacitanie udajov pre profilovky
+var totoTrebaZatvorit, aktualneOkno;
+var zdroj,nazov,popis;
+$.getJSON("../profilovky.json", function (json) {
+    console.log(json.profilovky.length);
+    for (i=0; i<json.profilovky.length; i++) {
+        zdroj = json.profilovky[i].src;
+        nazov = json.profilovky[i].title;
+        popis = json.profilovky[i].description;
+        console.log(zdroj);
+        console.log(nazov);
+        vytvorObrazok(zdroj,nazov,i,popis);
+    }
+
+});
+
+
+function vytvorObrazok(zdroj,nazov,id, popis) {
+    console.log(zdroj);
+    console.log(nazov);
+    var x = document.createElement("img");
+    var y = document.createElement("div");
+    var nadpis = document.createElement("b");
+    nadpis.append(nazov);
+    y.setAttribute("class","col-lg-4 meno")
+    x.setAttribute("src", zdroj);
+    x.setAttribute("alt", nazov);
+    originalOkno(zdroj,nazov,popis)
+
+    x.onclick = function(){
+
+        otvorOkno(nazov);
+        totoTrebaZatvorit = nazov;
+        aktualneOkno = id;
+
+
+    };
+    document.getElementById("profilovky").appendChild(y).appendChild(nadpis);
+    document.getElementById("profilovky").appendChild(y).appendChild(x);
+
+}
+
+function otvorOkno(nazov){
+    document.getElementById("okno").style.display="block";
+    document.getElementById(nazov).style.display="block";
+}
+function zatvorOkno(){
+
+    document.getElementById(totoTrebaZatvorit).style.display = "none";
+    document.getElementById("okno").style.display = "none";
+}
+
+
+
+function originalOkno(zdroj,nazov,popis){
+
+    var divko = document.createElement("div");
+    var nadpis = document.createElement("h3");
+    var obsah = document.createElement("p");
+    var original = document.createElement("img");
+    divko.setAttribute("id", nazov);
+    original.setAttribute("src", zdroj);
+    original.setAttribute("alt", nazov);
+    original.setAttribute("class", "obrazok");
+    nadpis.innerHTML = nazov;
+    obsah.innerHTML = popis;
+    divko.appendChild(nadpis);
+    divko.appendChild(obsah);
+    divko.appendChild(original);
+    document.getElementById("okno").appendChild(divko);
+    document.getElementById(nazov).style.display="none";
+
+
+}
