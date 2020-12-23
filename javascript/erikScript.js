0af15500f7f932c74f2fe0efb87e2a24c2a569f0
erik-ctrl <richtererik20@gmail.com>
2020-12-23 17:54:54 +0100

    First version of my game

--- javascript/erikScript.js
+++ javascript/erikScript.js
@@ -1,4 +1,5 @@
 console.log("TEST OK");
+var counting = 0;
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -8,8 +9,36 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
+function drop(ev, id) {
+
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var currentPosition = obj_transform(data);
+
+
+    if(isComponentFit(id, data) === true){
+        ev.target.appendChild(document.getElementById(data));
+        changeStyle(currentPosition);
+        counting++;
+    }
+
+    if(counting === 9){
+        document.getElementById("congrats").style.display = 'block';
+    }
+}
+
+function obj_transform(string){
+    return "position" + string.slice(6);
+}
+
+function changeStyle(elementID){
+    var newStyle = document.getElementById(elementID).style;
+    newStyle.opacity = '100%';
+}
+
+function isComponentFit(position, component){
+    var sPosition = position.slice(8);
+    var sComponent = component.slice(6);
+
+    return sPosition === sComponent;
 }
