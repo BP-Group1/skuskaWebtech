59df61b04d2b6647bfe22f275a4c9c3f21165028
pribula329 <72249242+pribula329@users.noreply.github.com>
2020-12-28 14:36:29 +0100

    Lukas hra presun + cas

--- javascript/lukasScript.js
+++ javascript/lukasScript.js
@@ -2,6 +2,9 @@
 
 });*/
 
+var pustiCas = 0;
+var spravnePolozene = 0;
+var spustene;
 function povolPustenie(ev) {
 
     ev.preventDefault();
@@ -9,6 +12,9 @@ function povolPustenie(ev) {
 
 function zoberObrazok(event) {
     event.dataTransfer.setData("text", event.target.id);
+    if (pustiCas == 0){
+        pocitajCas();
+    }
 }
 
 function pustiObrazok(ev,id) {
@@ -19,6 +25,47 @@ function pustiObrazok(ev,id) {
     console.log(id);
     if (kontrolaPozicie==id){
         ev.target.appendChild(document.getElementById(data));
+        spravnePolozene = spravnePolozene + 1;
+    }
+
+    if(spravnePolozene == 10)
+    {
+        clearTimeout(spustene);
     }
 
 }
+
+var sekundy = 0;
+var minuty = 0;
+
+function cas() {
+    sekundy = sekundy + 1;
+    if (sekundy >= 60){
+        sekundy = 0;
+        minuty = minuty + 1;
+    }
+
+    zobrazCas(sekundy,minuty);
+
+}
+
+function zobrazCas(sekundy, minuty){
+    if (sekundy<10)
+    {
+        sekundy="0" + sekundy;
+    }
+
+    if (minuty<10)
+    {
+        minuty="0" + minuty;
+    }
+    var casDisplay = "00:" + minuty + ":" + sekundy;
+    document.getElementById("cas").innerText = casDisplay;
+}
+
+function pocitajCas(){
+    pustiCas = 1;
+    cas();
+    spustene = setTimeout('pocitajCas();',1000)
+}
+
