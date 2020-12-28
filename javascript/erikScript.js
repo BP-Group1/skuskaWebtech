45805710ba68695d313fe96d4d6b34f08ae04a24
erik-ctrl <richtererik20@gmail.com>
2020-12-28 14:34:35 +0100

    Tooltips on subpages
    Pridal som tooltipy do kazdej podstranky

--- javascript/erikScript.js
+++ javascript/erikScript.js
@@ -42,3 +42,23 @@ function isComponentFit(position, component){
 
     return sPosition === sComponent;
 }
+
+
+document.getElementById("eGame").addEventListener('mousemove', function (){
+    document.getElementById("stiloImgE").style.display = "block";
+});
+document.getElementById("eGame").addEventListener('mouseleave', function (){
+    document.getElementById("stiloImgE").style.display = "none";
+});
+document.getElementById("lGame").addEventListener('mousemove', function (){
+    document.getElementById("transformersImgE").style.display = "block";
+});
+document.getElementById("lGame").addEventListener('mouseleave', function (){
+    document.getElementById("transformersImgE").style.display = "none";
+});
+document.getElementById("pGame").addEventListener('mousemove', function (){
+    document.getElementById("counterStrikeImgE").style.display = "block";
+});
+document.getElementById("pGame").addEventListener('mouseleave', function (){
+    document.getElementById("counterStrikeImgE").style.display = "none";
+});
--- javascript/peterGame.js
+++ javascript/peterGame.js
@@ -137,3 +137,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 */
+
+document.getElementById("eGame").addEventListener('mousemove', function (){
+    document.getElementById("stiloImgP").style.display = "block";
+});
+document.getElementById("eGame").addEventListener('mouseleave', function (){
+    document.getElementById("stiloImgP").style.display = "none";
+});
+document.getElementById("lGame").addEventListener('mousemove', function (){
+    document.getElementById("transformersImgP").style.display = "block";
+});
+document.getElementById("lGame").addEventListener('mouseleave', function (){
+    document.getElementById("transformersImgP").style.display = "none";
+});
+document.getElementById("pGame").addEventListener('mousemove', function (){
+    document.getElementById("counterStrikeImgP").style.display = "block";
+});
+document.getElementById("pGame").addEventListener('mouseleave', function (){
+    document.getElementById("counterStrikeImgP").style.display = "none";
+});
